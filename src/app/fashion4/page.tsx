d945f78619ff8c829e0d6a8255ff565421a3b139
williamuic <1187541557@qq.com>
2025-07-15 14:54:59 +0100

    add fashion

---
+++ src/app/fashion4/page.tsx
@@ -0,0 +1,7 @@
+export default function Fashion4() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-3xl font-bold">Fashion 4 Page Coming Soon</h1>
+    </div>
+  );
+} 
